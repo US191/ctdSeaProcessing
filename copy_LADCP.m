96aab3dc1e368ad35f1970ec1490e01111bdfe78
Jacques Grelet <Jacques.Grelet@ird.fr>
2017-02-09 15:39:42 +0100

    new files

---
+++ copy_LADCP.m
@@ -0,0 +1,134 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Preprocessing software for CTD-LADCP                                     %
+% Autor: Pierre Rousselot / Date: 10/03/16                                 %
+% Jedi master: Jacques Grelet                                              %
+% -> Copy LADCP acquisition file to processing path                        %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [ind_error] = copy_LADCP(cfg, logfile)
+
+%--------------------------------------------------------------------------
+% error indicative
+ind_error = 0; 
+
+% Copy LADCP files 
+fileLADCPMoutput = sprintf('%s', cfg.path_output_LADCP, cfg.filename_LADCPM);
+fileLADCPSoutput = sprintf('%s', cfg.path_output_LADCP, cfg.filename_LADCPS);
+newfileLADCPMoutput = sprintf('%s', cfg.path_save_LADCP, cfg.newfilename_LADCPM);
+newfileLADCPSoutput = sprintf('%s', cfg.path_save_LADCP, cfg.newfilename_LADCPS);
+
+disp(' '); disp('LADCP PROCESSING'); 
+fprintf(logfile, '\n LADCP PROCESSING \n');
+
+if exist(fileLADCPMoutput,'file') && exist(fileLADCPSoutput,'file')
+
+    textlog = sprintf('COPY LADCP RAW FILES');
+    write_logfile(logfile, textlog);
+    
+    textlog = sprintf('    %s and %s \n to \n    %s and %s',...
+        fileLADCPMoutput, fileLADCPSoutput, newfileLADCPMoutput, newfileLADCPSoutput);
+    
+    if cfg.debug_mode
+        
+        write_logfile(logfile, textlog);
+    
+    else
+        
+        write_logfile(logfile, textlog);
+        
+        copyfile (fileLADCPMoutput, newfileLADCPMoutput);
+        copyfile (fileLADCPSoutput, newfileLADCPSoutput);
+    
+    end
+    
+else
+ 
+    texterror = sprintf('>   !!! LADCP files %s and %s are missing', fileLADCPMoutput, fileLADCPSoutput);
+    ind_error = 1;
+    
+    if cfg.debug_mode
+        
+        error_logfile (logfile, texterror)
+    
+    else
+        
+        error_logfile (logfile, texterror)
+        msgbox({'LADCP files are missing !'...
+            'Please verify files have been extracted with BBTalk'}, 'Error', 'error')
+        
+    end
+
+end
+
+%--------------------------------------------------------------------------
+% Copy raw-data on the network
+
+textlog = sprintf ('COPY LADCP RAW FILES ON THE NETWORK');
+write_logfile(logfile, textlog);
+
+textlog = sprintf (' then to \n    %s \n    %s',...
+    cfg.path_raw_LADCP, cfg.path_processing_LADCP);
+
+if cfg.debug_mode
+        
+    write_logfile(logfile, textlog);
+    
+else
+    
+    write_logfile(logfile, textlog);
+
+    copyfile (newfileLADCPMoutput, cfg.path_raw_LADCP);
+    copyfile (newfileLADCPSoutput, cfg.path_raw_LADCP);
+    copyfile (newfileLADCPMoutput, cfg.path_processing_LADCP);
+    copyfile (newfileLADCPSoutput, cfg.path_processing_LADCP);
+
+end
+
+%--------------------------------------------------------------------------
+% End of the process
+newfileLADCPMraw = sprintf('%s', cfg.path_raw_LADCP, cfg.newfilename_LADCPM);
+newfileLADCPSraw = sprintf('%s', cfg.path_raw_LADCP, cfg.newfilename_LADCPS);
+newfileLADCPMprocess = sprintf('%s', cfg.path_processing_LADCP, cfg.newfilename_LADCPM);
+newfileLADCPSprocess = sprintf('%s', cfg.path_processing_LADCP, cfg.newfilename_LADCPS);
+
+if exist(newfileLADCPMraw,'file') && exist(newfileLADCPSraw,'file')...
+        && exist(newfileLADCPMprocess,'file') && exist(newfileLADCPSprocess,'file')
+
+    textlog = sprintf('END OF LADCP COPY PROCESS');
+    
+    write_logfile (logfile, textlog);
+
+else
+
+    texterror = sprintf('>   !!! Problem for copying LADCP files');
+    ind_error = 1;
+    
+    if cfg.debug_mode
+    
+        error_logfile (logfile, texterror)
+    
+    else
+        
+        error_logfile (logfile, texterror)
+        msgbox({'A problem occured during LADCP files copying !'...
+            'Please verify the paths and the filenames'}, 'Error', 'error')    
+    
+    end
+
+end
+
+%--------------------------------------------------------------------------
+    function write_logfile (logfile, textlog)
+    
+        disp(textlog); 
+        fprintf(logfile,'%s \n', textlog);
+
+    end
+
+    function error_logfile (logfile, texterror)
+        
+        disp(texterror);
+        fprintf(logfile, '%s \n', texterror);
+        
+    end
+
+end
---
+++ process_LADCP.m
@@ -0,0 +1,47 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Preprocessing software for CTD-LADCP                                     %
+% Autor: Pierre Rousselot / Date: 10/03/16                                 %
+% Jedi master: Jacques Grelet                                              %
+% -> LADCP processing                                                      %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function process_LADCP(cfg, logfile)
+close all
+
+%% LADCP file LDEO processing
+% Initialisation
+disp(' '); textlog = sprintf('INITIALIZING LDEO LADCP PROCESSING');
+logfile_CTD = logfile; %Resolve conflict LDEO Processing/ctdSeaProcessing
+
+if cfg.debug_mode
+    
+    write_logfile_CTD (logfile_CTD, textlog);
+    
+else
+    
+    write_logfile_CTD (logfile_CTD, textlog);
+    fclose('all')
+    
+    cd(cfg.process_LDEO)
+    addpath(genpath(cfg.process_LDEO));
+    addpath(cfg.drive)
+    set(0,'defaultsurfaceedgecolor', 'none');
+    set(0,'defaultfigurerenderer', 'painter');
+
+    % Processing
+    disp('LDEO LADCP Processing');
+    process_cast(cfg.num_station)
+
+end
+
+textlog = sprintf('END OF LADCP PROCESS');
+disp(textlog);
+
+%--------------------------------------------------------------------------
+    function write_logfile_CTD (logfile_CTD, textlog)
+    
+        disp(textlog); 
+        fprintf(logfile_CTD,'%s \n', textlog);
+
+    end
+
+end
