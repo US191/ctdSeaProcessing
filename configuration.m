1e5509792d2dc1c87094cdf7a058ee9e06caf5e7
Prousselot <pierre.rousselot@ird.fr>
2019-10-21 11:31:06 +0200

    Debug help on main windows + add SBE35 option and LADCP exception if files don't exist in configuration.m

--- configuration.m
+++ configuration.m
@@ -31,25 +31,25 @@ cfg.process_LDEO            = sprintf('%s', cfg.drive, filesep, cfg.name_mission
 
 % convert to Boolean
 cfg.copy_CTD                = str2num(cfg.copy_CTD);
+cfg.copy_SBE35              = str2num(cfg.copy_sbe35);
 cfg.copy_LADCP              = str2num(cfg.copy_LADCP);
 cfg.process_CTD             = str2num(cfg.process_CTD);
 cfg.process_LADCP           = str2num(cfg.process_LADCP);
 cfg.process_PMEL            = str2num(cfg.process_PMEL);
 cfg.process_BTL             = str2num(cfg.process_BTL);
 
-% find filename ADCP
+% find filename LADCP
 if cfg.process_LADCP
   master_file = fopen(sprintf('%s', cfg.path_LADCP, filesep, cfg.name_mission, filesep, 'MASTER.TXT'), 'r');
   slave_file = fopen(sprintf('%s', cfg.path_LADCP, filesep, cfg.name_mission, filesep, 'SLAVE.TXT'), 'r');
-  M = fscanf(master_file, '%s');
-  S = fscanf(slave_file, '%s');
-  cfg.filename_LADCPM = [M(strfind(M, 'RN')+2:strfind(M, 'RN')+6),'000.000'];
-  cfg.filename_LADCPS = [S(strfind(S, 'RN')+2:strfind(S, 'RN')+6),'000.000'];
+  if master_file ~= -1 || slave_file ~= -1
+      M = fscanf(master_file, '%s');
+      S = fscanf(slave_file, '%s');
+      cfg.filename_LADCPM = [M(strfind(M, 'RN')+2:strfind(M, 'RN')+6),'000.000'];
+      cfg.filename_LADCPS = [S(strfind(S, 'RN')+2:strfind(S, 'RN')+6),'000.000'];
+  else
+      msgbox('MASTER.TXT & SLAVE.TXT files doesn''t exist ! Are you sure you want to process LADCP files?', 'Warn', 'error');
+  end
 end
-
-% clear unused variable
-%clear cfg.rep_raw_CTD cfg.rep_processing_CTD cfg.rep_raw_LADCP cfg.rep_processing_LADCP cfg.rep_LDEO
-%clear cfg.rep_processing_raw_CTD cfg.rep_output_CTD cfg.rep_batch cfg.rep_codac cfg.rep_reports cfg.rep_output_LADCP cfg.rep_save_LADCP
-
 
 end
